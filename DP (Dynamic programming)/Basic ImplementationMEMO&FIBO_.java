7a82b12f2b54b653ae78b37a7a4c06415e6ca0d1
Prit Gondaliya <96789149+pritGondaliyav@users.noreply.github.com>
2022-03-30 16:07:06 +0530

    Update Basic ImplementationMEMO&FIBO_.java

--- DP (Dynamic programming)/Basic ImplementationMEMO&FIBO_.java
+++ DP (Dynamic programming)/Basic ImplementationMEMO&FIBO_.java
@@ -55,6 +55,7 @@ dp(perametrs,  perametrs ){
 
 *Fibonacci  is a Parent Qes *
 * stairCase  is a child Qes  This Qes ask in interview *
+  --> https://www.geeksforgeeks.org/count-ways-reach-nth-stair-using-step-1-2-3/
 
 
 =======================Fibonacci Of N Numberusing recursion =======================
