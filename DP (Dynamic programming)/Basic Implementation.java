4e04831373ceec4784de3ee2a07069e6082870d0
Prit Gondaliya <96789149+pritGondaliyav@users.noreply.github.com>
2022-03-30 12:24:00 +0530

    Update and rename Basic Implementation to Basic Implementation.java

---
+++ DP (Dynamic programming)/Basic Implementation.java
@@ -0,0 +1,17 @@
+/*1) What is  Dynamic programming ?
+ --> Dynamic programming is a technique that breaks the problems into sub-problems, and saves the result for future purposes so that we do not need to compute the result again 
+ --> Dp is tack bruteforce and it optimize it 
+
+Some time some Qes Coding is problomatic  
+becuse we redo stap again & again 
+we need to avoid repeting think we done earlier 
+like:-
+ 1 + 1 + 1 + 1 + 1  = 6 
+if we store sum and use agin in other opration than we get ans faster 
+
+
+*/
+
+
+
+
