160d20d1a4eae19c43742cadef444a5ac3d05dd8
Prit Gondaliya <96789149+pritGondaliyav@users.noreply.github.com>
2022-03-31 13:11:56 +0530

    Update Knapsack_Pro.java

--- DP (Dynamic programming)/DAY 2/Knapsack_Pro.java
+++ DP (Dynamic programming)/DAY 2/Knapsack_Pro.java
@@ -1,6 +1,30 @@
 /*
-Qes : DP (Dynamic programming)/DAY 2/problemStatment.jpeg
-1)  You need to put item array to bag such that the bag has max price value 
+Qes 1) 
+https://github.com/pritGondaliyav/Advanced-Data-Structures/blob/main/DP%20(Dynamic%20programming)/DAY%202/problemStatment.jpeg
+DP (Dynamic programming)/DAY 2/problemStatment.jpeg --> You need to put item array to bag such that the bag has max price value 
 
+This is a Optimazation  Problem 
 
+Ans : 
+  --> Choice Diagram 
+  For each item we can  Make Two Choice 
+    
+                  Item
+                 /    \
+                /      \
+            Put item    Not put this 
+            in bag      item  in bag
+                              /\
+                             /  \
+                            /    \
+                    the weight      Skipping it my own    
+                    of item more 
+                    than bag 
+                    capacity left
+                    
+                    
+                    
+                    
+                    
+                    
 */
